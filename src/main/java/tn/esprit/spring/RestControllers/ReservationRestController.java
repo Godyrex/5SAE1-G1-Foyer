fd0a9ccc590b9ed6f13c105fb62fb2d282acf3eb
Nour-Challouf <115362461+nour-challouf@users.noreply.github.com>
2024-11-06 01:15:57 +0100

    resevationControllerUpdate

--- src/main/java/tn/esprit/spring/RestControllers/ReservationRestController.java
+++ src/main/java/tn/esprit/spring/RestControllers/ReservationRestController.java
@@ -10,44 +10,7 @@ import tn.esprit.spring.Services.Universite.IUniversiteService;
 import java.time.LocalDate;
 import java.util.List;
 
-@Rversion: '3.8'
-
-        services:
-        database:
-        image: mysql:5.7
-        container_name: mysql_db
-        environment:
-        MYSQL_ROOT_PASSWORD: root_password # Change this to a secure password
-        MYSQL_DATABASE: foyer
-        MYSQL_USER: root
-        MYSQL_PASSWORD: root_password
-        ports:
-        - "3306:3306"
-        volumes:
-        - db_data:/var/lib/mysql
-
-        app:
-        image: openjdk:17-jdk-alpine
-        container_name: spring_app
-        environment:
-        SPRING_DATASOURCE_URL: jdbc:mysql://database:3306/foyer?createDatabaseIfNotExist=true&useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC
-        SPRING_DATASOURCE_USERNAME: root
-        SPRING_DATASOURCE_PASSWORD: root_password
-        SPRING_JPA_HIBERNATE_DDL_AUTO: update
-        SERVER_PORT: 8081
-        SERVER_SERVLET_CONTEXT_PATH: /Foyer
-        depends_on:
-        - database
-        ports:
-        - "8081:8081"
-        volumes:
-        - ./target:/app
-        working_dir: /app
-        command: ["java", "-jar", "Foyer-0.0.1-SNAPSHOT.jar"]
-
-        volumes:
-        db_data:
-        estController
+@RestController
 @RequestMapping("reservation")
 @AllArgsConstructor
 public class ReservationRestController {
